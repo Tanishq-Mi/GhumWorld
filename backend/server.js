ae7a3ac532e4919e086a0387597ae5485eceb224
Tanishq Mishra <143572288+Tanishq-Mi@users.noreply.github.com>
2026-06-17 19:27:22 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,10 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:3000', 'http://localhost:5173'],
+  origin: [ 'http://localhost:3000',
+    'http://localhost:5173',
+    'http://localhost:5174',
+    'https://ghum-world-8ica.vercel.app'],
   credentials: true
 }));
 app.use(express.json());
